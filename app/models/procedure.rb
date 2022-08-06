5475e02250fd44bc75c51b4f4c8b4763440fd9bf
Rajan Ladwa <rladwa@gmail.com>
2022-08-06 14:39:08 +0900

    Numerous nodel/controller/schema updates

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,4 +1,4 @@
 class Procedure < ApplicationRecord
-  has_many :steps
+  has_many :consultations
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_06_045456) do
+ActiveRecord::Schema.define(version: 2022_08_06_053426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,20 +49,10 @@ ActiveRecord::Schema.define(version: 2022_08_06_045456) do
 
   create_table "steps", force: :cascade do |t|
     t.string "name"
-    t.bigint "procedure_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["procedure_id"], name: "index_steps_on_procedure_id"
-  end
-
-  create_table "submissions", force: :cascade do |t|
+    t.text "instruction"
     t.integer "status"
-    t.bigint "step_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["step_id"], name: "index_submissions_on_step_id"
-    t.index ["user_id"], name: "index_submissions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -82,7 +72,4 @@ ActiveRecord::Schema.define(version: 2022_08_06_045456) do
   end
 
   add_foreign_key "lawfirms", "users"
-  add_foreign_key "steps", "procedures"
-  add_foreign_key "submissions", "steps"
-  add_foreign_key "submissions", "users"
 end
