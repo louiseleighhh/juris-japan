0e7b7628d39ff5644bc76263ff7987e523751279
Michelle Barrie <michelle@barrie.cl>
2022-08-06 12:24:55 +0900

    creater new and create methods for procedures, steps and submissions

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,2 +1,21 @@
 class ProceduresController < ApplicationController
+
+  def new
+    @procedure = Procedure.new
+  end
+
+  def create
+    @procedure = Procedure.new(procedure_params)
+    if @procedure.save
+      redirect_to procedures_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def procedure_params
+    params.require(:procedure).permit(:name)
+  end
 end
--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,2 +1,24 @@
 class StepsController < ApplicationController
+
+  def new
+    @step = Step.new
+    @procedure = Procedure.find(params[:procedure_id])
+  end
+
+  def create
+    @step = Step.new(step_params)
+    @procedure = Procedure.find(params[:procedure_id])
+    @submission.procedure = @procedure
+    if @submission.save
+      redirect_to procedures_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(:name)
+  end
 end
