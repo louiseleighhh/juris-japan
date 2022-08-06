c67ff3ec4ba8c829d5952a4e3459895b9ec3ad08
louiseleighhh <louisecapala@gmail.com>
2022-08-06 14:45:30 +0900

    added edit and update on messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,27 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:update, :edit]
   def index
     @messages = Message.all
   end
 
   def show
+    @message = Message.find(params[:id])
+  end
+
+  def edit; end
+
+  def update
+    @message.update(message_params)
+    redirect_to message_path(@message)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def set_message
     @message = Message.find(params[:id])
   end
 end
