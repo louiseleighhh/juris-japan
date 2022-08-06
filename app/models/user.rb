037b9fc4cc77e53196f578fda0919bb93bdb92d2
Rajan Ladwa <rladwa@gmail.com>
2022-08-06 13:57:44 +0900

    User model - updated schema and added validations/associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :consultations, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { in: 6..20 }
+  validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "Letters only" }
+  validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "Letters only" }
 end
