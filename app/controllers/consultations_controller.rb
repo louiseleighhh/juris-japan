b307e0e9fd1cf2fd829820c0ed846b629e5834bf
louiseleighhh <louisecapala@gmail.com>
2022-08-06 14:25:41 +0900

    finished edit and update for consultation

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,10 +1,31 @@
 class ConsultationsController < ApplicationController
+  before_action :set_consultation, only: [:update, :edit]
+
   def index
     @consultations = Consultation.all
     # Consultation.where(user: current_user)
   end
 
   def show
+    @consultation = Consultation.find(params[:id])
+  end
+
+  def edit; end
+
+  def update
+    @lawfirm = Lawfirm.find(params[:lawfirm_id])
+    @consultation.lawfirm = @lawfirm
+    @consultation.update
+    redirect_to consultation_path(@consultation)
+  end
+
+  private
+
+  # def consultation_params
+  #   params.require(:consultation).permit()
+  # end
+
+  def set_consultation
     @consultation = Consultation.find(params[:id])
   end
 end
