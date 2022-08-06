5c6b75ca52ef39196931f7f7353a7f72869ed358
louiseleighhh <louisecapala@gmail.com>
2022-08-06 12:10:53 +0900

    did the general index and show

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,2 +1,10 @@
 class ConsultationsController < ApplicationController
+  def index
+    @consultations = Consultation.all
+    # Consultation.where(user: current_user)
+  end
+
+  def show
+    @consultation = Consultation.find[params[:id]]
+  end
 end
--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,9 @@
 class SubmissionsController < ApplicationController
+  def index
+    @submissions = Submission.all
+  end
+
+  def show
+    @submission = Submission.find[params[:id]]
+  end
 end
