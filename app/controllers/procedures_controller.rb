c6773eed6d3f237e2c0f647ee96ccefe2517d8d9
Michelle Barrie <michelle@barrie.cl>
2022-08-06 17:06:44 +0900

    updated controllers based on new routes

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -7,31 +7,31 @@ class ProceduresController < ApplicationController
     @procedure = Procedure.find(params[:id])
   end
 
-  def new
-    @procedure = Procedure.new
-  end
+  #def new
+  #  @procedure = Procedure.new
+  #end
 
-  def create
-    @procedure = Procedure.new(procedure_params)
-    if @procedure.save
-      redirect_to procedures_path
-    else
-      render :new
-    end
-  end
+  #def create
+  #  @procedure = Procedure.new(procedure_params)
+  #  if @procedure.save
+  #    redirect_to procedures_path
+  #  else
+  #    render :new
+  #  end
+  #end
 
-  def edit
-    @procedure = Procedure.find(params[:id])
-  end
+  #def edit
+  #  @procedure = Procedure.find(params[:id])
+  #end
 
-  def update
-    @procedure = Procedure.find(params[:id])
-    @procedure.update(procedure_params)
-    redirect_to procedure_path(@procedure)
+  #def update
+  #  @procedure = Procedure.find(params[:id])
+  #  @procedure.update(procedure_params)
+  #  redirect_to procedure_path(@procedure)
     # or maybe to procedures_path ?
     # Not sure if you prefer to refirect to procedures_path
 
-  end
+  #end
 
   private
 
