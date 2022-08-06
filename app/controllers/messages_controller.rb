2916a56ae88ed69a4b0d33a3c0ae59ef2651531e
Michelle Barrie <michelle@barrie.cl>
2022-08-06 14:43:49 +0900

    added new and create in message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,32 @@ class MessagesController < ApplicationController
     @messages = Message.all
   end
 
+  def new
+    @message = Message.new
+    @consultation = Consultation.find(params[:consultation_id])
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @user = current_user
+    @message.user = @user
+    @consultation = Consultation.find(params[:consultation_id])
+    @message.consultation = @consultation
+    if @message.save
+      redirect_to message_path(@message)
+      # Not sure about the routes here
+    else
+      render :new
+    end
+  end
+
   def show
     @message = Message.find(params[:id])
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
