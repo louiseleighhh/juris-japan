514c765a284489f54a413c1030a1ece974ed4836
Michelle Barrie <michelle@barrie.cl>
2022-08-11 22:13:13 +0900

    added the see more display

---
+++ app/javascript/controllers/seemore_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static targets = [ 'content' ]
+
+  connect() {
+    console.log('Hello from seemore_controller.js')
+    // console.log(this.testTarget)
+  }
+
+  display(event) {
+    const seemoreContent = this.contentTarget
+    const button = event.currentTarget
+
+      button.classList.toggle('seemore__button--active');
+
+      if (button.classList.contains('seemore__button--active')) {
+        seemoreContent.style.maxHeight = seemoreContent.scrollHeight + 'px';
+      } else {
+        seemoreContent.style.maxHeight = 0;
+      }
+  }
+}
