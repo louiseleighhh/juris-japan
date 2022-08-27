12859993bd0df000b7178d4fc17eb6d28d6a2c6f
Michelle Barrie <michelle@barrie.cl>
2022-08-27 18:13:02 +0900

    Checkboxes automatically check/uncheck once user uploads documents. Progress bar is automated based on the completition state of each step, chanching colors and label

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,4 +2,9 @@ class Step < ApplicationRecord
   belongs_to :consultation
   has_many :items, dependent: :destroy
   validates :name, :instruction, presence: true
+  def completed?
+    items.all? do |item|
+      item.checked?
+    end
+  end
 end
