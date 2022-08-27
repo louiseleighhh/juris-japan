4524180d0431194e65701baadf0af0b4b90a7534
Sara Jessome <sajessome@gmail.com>
2022-08-27 13:39:13 +0900

    Removed consultation_id string from messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_27_043531) do
+ActiveRecord::Schema.define(version: 2022_08_27_043811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,7 +103,6 @@ ActiveRecord::Schema.define(version: 2022_08_27_043531) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.string "consultation_id"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
