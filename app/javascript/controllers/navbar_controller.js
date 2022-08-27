dd05ffcbc93f66d38a5f661ff421850993106afc
louiseleighhh <louisecapala@gmail.com>
2022-08-27 12:22:21 +0900

    made the navbar non-transparent instantly and fixed steps button

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
     const navBar = document.getElementById("top-banner");
     const logoPic = document.getElementById("logo");
     const dropDown = document.getElementById("dropdown")
-    if (window.scrollY >= navBar.scrollHeight) {
+    if (window.scrollY >= 50) {
       this.element.classList.add("navbar-lewagon-blue")
       dropDown.classList.add("dropdown-menu-blue")
     } else {
