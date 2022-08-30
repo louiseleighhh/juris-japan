4f5e3323b24b6ab9b317935567ddb9bdb87ea4f0
Sara Jessome <sajessome@gmail.com>
2022-08-30 21:47:28 +0900

    Updated seeds.rb to produce 15 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,15 +28,15 @@ user = User.create(
 puts 'Test user created...'
 
 puts 'Creating procedures...'
-  procedure = Procedure.create(
-    name: "PR"
-    )
-    puts "Created procedure #{procedure.id}"
+procedure = Procedure.create(
+  name: "Permanent Residence"
+)
+puts "Created procedure #{procedure.id}"
 
-    # seed the steps
+# Seed the steps
 
-    puts 'Creating users...'
-25.times do
+puts 'Creating users...'
+15.times do
   user = User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -44,6 +44,7 @@ puts 'Creating procedures...'
     password: '1234567',
     location: ["Tokyo", "Osaka", "Kyoto", "Chiba", "Ibaraki", "Yokohama", "Nagoya", "Shizuoka"].sample
   )
+
   puts "Created user #{user.id}"
 
   step_path = Rails.root.join
@@ -53,101 +54,102 @@ puts 'Creating procedures...'
     procedure: procedure
   )
 
-  step1 = Step.create(
-    name: "1 - Application Form",
-    consultation: consultation,
-    instruction: "Please fill in the application form (永住許可申請書) and upload the form"
-  )
+  # step1 = Step.create(
+  #   name: "1 - Application Form",
+  #   consultation: consultation,
+  #   instruction: "Please fill in the application form (永住許可申請書) and upload the form"
+  # )
 
-  item1 = Item.create(
-    name: "Application Form",
-    description: File.open("#{step_path}/items/item1.md").read,
-    step: step1
-  )
+  # item1 = Item.create(
+  #   name: "Application Form",
+  #   description: File.open("#{step_path}/items/item1.md").read,
+  #   step: step1
+  # )
 
-  step2 = Step.create(
-    name: "2 - Upload a photograph",
-    consultation: consultation,
-    instruction: "Please upload photograph based on MOJ website requirements"
-  )
+  # step2 = Step.create(
+  #   name: "2 - Upload a photograph",
+  #   consultation: consultation,
+  #   instruction: "Please upload photograph based on MOJ website requirements"
+  # )
 
-  item2 = Item.create(
-    name: "Upload a photograph",
-    description: File.open("#{step_path}/items/item2.md").read,
-    step: step2
-  )
+  # item2 = Item.create(
+  #   name: "Upload a photograph",
+  #   description: File.open("#{step_path}/items/item2.md").read,
+  #   step: step2
+  # )
 
-  step3 = Step.create(
-    name: "3 - Proof of Employment Status",
-    consultation: consultation,
-    instruction: "Provide 1 copy of the following documents:"
-  )
+  # step3 = Step.create(
+  #   name: "3 - Proof of Employment Status",
+  #   consultation: consultation,
+  #   instruction: "Provide 1 copy of the following documents:"
+  # )
 
-  item3_1 = Item.create(
-    name: "If employed - Provide Certificate of Employment",
-    description: File.open("#{step_path}/items/item3d_1.md").read,
-    step: step3
-  )
+  # item3_1 = Item.create(
+  #   name: "If employed - Provide Certificate of Employment",
+  #   description: File.open("#{step_path}/items/item3d_1.md").read,
+  #   step: step3
+  # )
 
-  item3d_2 = Item.create(
-    name: "If self employed - Provide copy of the applicant's tax return, or the corporations' certificate of registered matters",
-    description: File.open("#{step_path}/items/item3d_2.md").read,
-    step: step3
-  )
+  # item3d_2 = Item.create(
+  #   name: "If self employed - Provide copy of the applicant's tax return, or the corporations' certificate of registered matters",
+  #   description: File.open("#{step_path}/items/item3d_2.md").read,
+  #   step: step3
+  # )
 
-  item3d_3 = Item.create(
-    name: "Other - Clarifying documents or other demonstrative materials which detail their employment (no specified format)",
-    description: File.open("#{step_path}/items/item3d_3.md").read,
-    step: step3
-  )
+  # item3d_3 = Item.create(
+  #   name: "Other - Clarifying documents or other demonstrative materials which detail their employment (no specified format)",
+  #   description: File.open("#{step_path}/items/item3d_3.md").read,
+  #   step: step3
+  # )
 
-  step4 = Step.create(
-    name: "4 - Residence Card",
-    consultation: consultation,
-    instruction: "Upload only if applying via a proxy, otherwise please produce your card at the immigration office"
-  )
+  # step4 = Step.create(
+  #   name: "4 - Residence Card",
+  #   consultation: consultation,
+  #   instruction: "Upload only if applying via a proxy, otherwise please produce your card at the immigration office"
+  # )
 
-  item4 = Item.create(
-    name: "Upload Residence Card",
-    description: File.open("#{step_path}/items/item4.md").read,
-    step: step4
-  )
+  # item4 = Item.create(
+  #   name: "Upload Residence Card",
+  #   description: File.open("#{step_path}/items/item4.md").read,
+  #   step: step4
+  # )
 
-  step5 = Step.create(
-    name: "5 - Written Permission",
-    consultation: consultation,
-    instruction: "Provide written permission to engage in an activity other than those permitted by the status of residence, previously granted (where applicable)"
-  )
+  # step5 = Step.create(
+  #   name: "5 - Written Permission",
+  #   consultation: consultation,
+  #   instruction: "Provide written permission to engage in an activity other than those permitted by the status of residence, previously granted (where applicable)"
+  # )
 
-  item5 = Item.create(
-    name: "Upload written permission",
-    description: File.open("#{step_path}/items/item5.md").read,
-    step: step5
-  )
+  # item5 = Item.create(
+  #   name: "Upload written permission",
+  #   description: File.open("#{step_path}/items/item5.md").read,
+  #   step: step5
+  # )
 
-  step6 = Step.create(
-    name: "6 - Passport / Certificate of Eligibility (COE)",
-    consultation: consultation,
-    instruction: "Upload only if applying via a proxy, otherwise please produce your passport or CoE at the immigration office"
-  )
+  # step6 = Step.create(
+  #   name: "6 - Passport / Certificate of Eligibility (COE)",
+  #   consultation: consultation,
+  #   instruction: "Upload only if applying via a proxy, otherwise please produce your passport or CoE at the immigration office"
+  # )
 
-  item6 = Item.create(
-    name: "Upload Passport or COE",
-    description: File.open("#{step_path}/items/item6.md").read,
-    step: step6
-  )
+  # item6 = Item.create(
+  #   name: "Upload Passport or COE",
+  #   description: File.open("#{step_path}/items/item6.md").read,
+  #   step: step6
+  # )
 
-  step7 = Step.create(
-    name: "7 - *Proxy Only* Documents certifying identity",
-    consultation: consultation,
-    instruction: "Please produce ID at immigration office"
-  )
+  # step7 = Step.create(
+  #   name: "7 - *Proxy Only* Documents certifying identity",
+  #   consultation: consultation,
+  #   instruction: "Please produce ID at immigration office"
+  # )
 
-  item7 = Item.create(
-    name: "Documents certifying identity",
-    description: File.open("#{step_path}/items/item7.md").read,
-    step: step7
-  )
+  # item7 = Item.create(
+  #   name: "Documents certifying identity",
+  #   description: File.open("#{step_path}/items/item7.md").read,
+  #   step: step7
+  # )
+
   random = rand(1..2)
   if random == 1
     puts 'Creating lawfirms...'
